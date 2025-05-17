2e2fb0423026275cc798754d7075fc862dfce065
ParKouRrr <cloverkitty8@gmail.com>
2025-05-17 14:33:34 -0400

    chequering

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,31 +4,35 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive.*;
+import edu.wpi.first.wpilibj.motorcontrol.*;
 
 public class DriveTrain extends SubsystemBase {
-  /** Creates a new DriveTrain. */
+  public DifferentialDrive drive; //its up here because if it's not it gets forgotten after the brackets end :,(
+  private final Joystick driverJoystick = new Joystick(0);
+  private final Joystick operatorJoystick = new Joystick(1);
+
   public DriveTrain() 
   {
     TalonSRX leftTalonSRX = new TalonSRX(3);
     TalonSRX rightTalonSRX = new TalonSRX(4);
-    //MotorController testMotorController()
-
-    //DifferentialDrive drive = new DifferentialDrive;
+    PWMTalonSRX testMotorControllerLeft = new PWMTalonSRX(3);
+    PWMTalonSRX testMotorControllerRight = new PWMTalonSRX(4);
+    drive = new DifferentialDrive(testMotorControllerLeft, testMotorControllerRight);
+  
   }
 
   @Override
-  public void periodic(
-
-  ) {
-    // This method will be called once per scheduler run
+  public void periodic() 
+    {
+    ArcadeDrive(driverJoystick.getRawAxis(0), driverJoystick.getRawAxis(4));
   }
   
-  public void arcadeDrive(double move, double rotate) {
-    //drive.arcadeDrive(move, rotate);
+  public void ArcadeDrive(double move, double rotate) {
+    drive.arcadeDrive(move, rotate);
   }
 }
