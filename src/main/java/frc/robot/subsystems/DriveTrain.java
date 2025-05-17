6821c66c197ae05b7a65e3381768acbd47d8ef5f
ParKouRrr <cloverkitty8@gmail.com>
2025-05-17 13:08:09 -0400

    guinea pig

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -13,10 +14,11 @@ public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
   public DriveTrain() 
   {
-    TalonSRX leftTalonSRX;
-    TalonSRX rightTalonSRX;
+    TalonSRX leftTalonSRX = new TalonSRX(3);
+    TalonSRX rightTalonSRX = new TalonSRX(4);
+    //MotorController testMotorController()
 
-    DifferentialDrive drive = new DifferentialDrive(leftTalonSRX, rightTalonSRX);
+    //DifferentialDrive drive = new DifferentialDrive;
   }
 
   @Override
@@ -27,6 +29,6 @@ public class DriveTrain extends SubsystemBase {
   }
   
   public void arcadeDrive(double move, double rotate) {
-    drive.arcadeDrive(move, rotate);
+    //drive.arcadeDrive(move, rotate);
   }
 }
